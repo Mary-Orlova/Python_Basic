ccf3e192ab9098807bc98afac70fe35ddb01eced
Maria Orlova <vampusha07@gmail.com>
2022-01-05 12:58:08 +0300

    Задача 4. Число наоборот 3

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,16 @@
-# TODO здесь писать код
+def opposite(num):
+    parts = str(num).split('.')
+    reversed_parts = [''.join(reversed(part)) for part in parts]
+
+    return float( '.'.join(reversed_parts) )
+
+n = opposite(input('Введите N: '))
+k = opposite(input('Введите K: '))
+
+print('Первое число наоборот:', n)
+print('Второе число наоборот:', k)
+print('Сумма:', n + k)
+
+
 
 
