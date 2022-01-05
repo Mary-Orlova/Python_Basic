72ebc7420ef9110f5cb078ea75cd6949e5e0fcea
Maria Orlova <vampusha07@gmail.com>
2022-01-05 13:13:15 +0300

    Задача 5. Наименьший делитель

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def find(number, nod):
+    while nod <= number:
+        nod = nod + 1
+        if number % nod == 0:
+            print('Наименьший делитель, отличный от единицы:', nod)
+            break
+
+
+number = int(input('Введите число:'))
+nod = 1
+find(number, nod)
