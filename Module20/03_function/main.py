5374b3378dd76b55735423ddc12384bbaf142a0f
Maria Orlova <vampusha07@gmail.com>
2022-04-21 16:02:27 +0300

    Базовые коллекции: Кортежи

--- Module20/03_function/main.py
+++ Module20/03_function/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+def find(test,element):
+    if element in test:
+        if test.count(element) > 1:
+            first_index = test.index(element)
+            second_index = test.index(element, first_index + 1) + 1
+            return test[first_index:second_index]
+        else:
+            return test[test.index(element):]
+    else:
+        return ()
+
+# print(find((1, 2, 3, 4, 5, 6, 7, 8, 2, 2, 9, 10), 2))
--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,5 @@
-# TODO здесь писать код
+def tpl_sort(numbers):
+    test = numbers[:]
+    return tuple(sorted(test))
+
+# print(tpl_sort((6,3,-1,8,4,10,-5)))
