d7faaab409095460a087858bd9a14b839438d2f3
Maria Orlova <vampusha07@gmail.com>
2022-05-29 19:27:25 +0300

    23 модуль - Исключения: работа с ошибками

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+sym_sum, line_count = 0, 0
+
+with open('people.txt', 'r') as people_file:
+    for line in people_file:
+        try:
+            lenght = len(line)
+            line_count += 1
+            if line.endswith('\n'):
+                lenght -= 1
+            sym_sum += lenght
+            if lenght < 3:
+                print('Ошибка: менее трёх символов в строке {} строке'.format(line_count))
+                raise BaseException
+        except FileNotFoundError:
+            print('Файл не найден')
+        except BaseException:
+            with open('error.log', 'w') as error_file:
+                error_file.write('Ошибка: менее трёх символов в строке {} строке'.format(line_count))
+
+print('Общее количество символов:', sym_sum)
+
