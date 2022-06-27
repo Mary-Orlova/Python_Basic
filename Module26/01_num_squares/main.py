e29731eefc8679eb15145681492c69ba301fe02a
Maria Orlova <vampusha07@gmail.com>
2022-06-27 17:12:28 +0300

    26 модуль - итераторы и генераторы

--- Module26/01_num_squares/main.py
+++ Module26/01_num_squares/main.py
@@ -1,1 +1,61 @@
-# TODO здесь писать код
+from typing import Any, Optional
+
+
+class CountIterator:
+    """Инициация класса итератор"""
+    def __init__(self, number: int):
+        """self.count = текущее перебираемое значение
+        self.number = передаваемое максимальное значение
+        """
+        self.count = 0
+        self.number = number
+
+    def __iter__(self) -> Optional[Any]:
+        """ Метод возврата итерируемого значения"""
+        return self
+
+    def __next__(self) -> int:
+        """Метод следующего итерируемого значения
+        Увеличиваем текущее значение на 1 единицу"""
+        self.count += 1
+        """ Проводим поверку - если текущее значение = максимальному,
+        то останавливаем данный метод, а иначе выводим на экран тек знач,
+         ктр возводим во вторую степень и возвращаем результ """
+        if self.count == self.number:
+            raise StopIteration
+        else:
+            print(self.count, '** 2', end=' ')
+            return self.count ** 2
+
+
+my_iter = CountIterator(number=6)
+
+print('Вывод квадратов - класс итератор')
+
+for element in my_iter:
+    """Класс- итератор -построчно вывод полученного значения из класса Итератор"""
+    print('=', element)
+
+
+def generator(number: int):
+    """Генератор квадратов от 1 до максимального"""
+    for number in range(1, number):
+        print(number, '** 2', end=' ')
+        yield number ** 2
+
+
+choose_number = int(input('\nВведите максимальное число: '))
+
+# второй способ
+generation = generator(choose_number)
+
+print('Вывод функции - генератор')
+for element in generation:
+    print('=', element)
+
+# третий способ
+print('\nВывод функции - генераторное выражение')
+generation_two = (element ** 2 for element in range(1, choose_number))
+
+for element in generation_two:
+    print(element)
--- Module26/05_str_count/main.py
+++ Module26/05_str_count/main.py
@@ -1,1 +1,48 @@
-# TODO здесь писать код
+import os
+from collections.abc import Iterable
+
+
+def gen_files_dir(path: str, depth=1) -> Iterable[str]:
+    """Рекурсивно перебираем файлы и каталоги до определенной глубины"""
+    depth -= 1
+    with os.scandir(path) as path_now:
+        for entry in path_now:
+            yield entry.path
+            if entry.is_dir() and depth > 0:
+                yield from gen_files_dir(entry.path, depth)
+
+
+directory = os.path.abspath(os.path.join('..', '..'))
+files = list(gen_files_dir(directory))
+line_count = 0
+
+for file_dir in files:
+    """Если "файл" директории не является файлом- продолжаем"""
+    if not os.path.isfile(file_dir):
+        continue
+
+    """Только файлы с расширением .py"""
+    files_filtered = [x for x in files if x.endswith('.py')]
+
+    if not files_filtered:
+        """Если "файл" не попал в фильтр файлов, то попробовать:
+        открыть в режиме чтения и вводим счетчик строки local_count
+        Для каждой строки в файле - если не пустая и не с комментарием и нет анотации, то
+        прибавляем 1 к счетчику строк
+        В конце файл закрываем
+        Если же поймана ошибка - продолжаем
+        """
+        try:
+            file = open(file_dir, 'r')
+            local_count = 0
+            for line in file.read().split('\n'):
+                if (not line.strip() == '') and (not line.strip().startswith("#")) and (
+                        not line.strip().startswith('"')):
+                    local_count += 1
+            print('\n{} - {} ст'.format(file_dir, local_count))
+            line_count += local_count
+            file.close()
+        except ValueError:
+            continue
+
+print('\nВсего строк:', line_count)
