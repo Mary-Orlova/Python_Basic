a66cf1bf21d3fc79c8998ca802faafa8fa43e401
Maria Orlova <vampusha07@gmail.com>
2022-07-23 02:16:23 +0300

    29 модуль - декораторы, декорирование классов, модификацию поведения внутри класса с помощью специальных функций.

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,45 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def check_permission(user_name: str) -> Callable:
+    """В user_permissions заносим имя пользователя - в основной программе присвоен admin """
+    # user_permissions = ['admin']
+
+    def check_permission_2(func: Callable) -> Callable:
+        """Метод проверки доступа к функции у текущего пользователя(user_permissions) """
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs) -> Callable:
+            try:
+                """Проверка и отлов ошибки:
+                если у пользователя есть доступ к вызываемой функции то - выполняется функция,
+                если нет - выдается исключение PermissionError"""
+                if user_name in user_permissions:
+                    return func(*args, **kwargs)
+                else:
+                    raise PermissionError
+            except PermissionError as error:
+                print(error)
+                print('У пользователя недостаточно прав, чтобы выполнить функцию {func_name}'.format(
+                    func_name=func.__name__))
+        return wrapped
+    return check_permission_2
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    """Метод для удаления сайта- только у администратора admin"""
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    """Метод добавления комментария - только у пользователя user_1"""
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+app = {}
+
+
+def callback(key: str):
+    """Метод - вызова функции,
+    в обертку передается функция example по ключу (//)
+    если передается другой ключ - то вызывается ошибка в 27 строке при проверке словаря
+    """
+    def wrapped(func: Callable) -> Any:
+        app[key] = func
+    return wrapped
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
