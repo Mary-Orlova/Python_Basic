729d4472d4f47c00eb58dcaad1d8ba3aebab70da
Maria Orlova <vampusha07@gmail.com>
2022-08-18 17:08:04 +0300

    Модуль 31: регулярные выражения, API, JSON, itertools upd: внесена конструкция if __name__ == main в 3,4,6 задачи

--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,11 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
-
+import re
 
 sample_web_page = 'http://www.columbia.edu/~fdc/sample.html'
 names = requests.get(sample_web_page)
 head_line = BeautifulSoup(names.text, 'lxml')
 print("Список заголовков h3 :")
 
-for heading in head_line.find_all(['h3']):
-    print( heading.text.strip())
+# for heading in head_line.find_all(['h3']):
+#     print( heading.text.strip())
+
+text = names.text
+result = re.findall(r'<h3 .*?>(.*?)</h3>', text)
+
+if __name__ == "__main__":
+    print(('\n'.join(result)))
